19400f3b2ee2f7ce3f59d8ac7a19b3cc0a3a5a30
Matthew-uk <esaduviedede@gmail.com>
2024-09-17 22:02:37 +0100

    Nothing MEGA

--- techmart/__init__.py
+++ techmart/__init__.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 def create_app():
 	app = Flask(__name__)
 	app.config['SECRET_KEY'] = 'zika1997'
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin@localhost/techmart'
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sql8731929:qiwYvQAVYT@sql8.freemysqlhosting.net/sql8731929'
 	
 	
 	db.init_app(app)
